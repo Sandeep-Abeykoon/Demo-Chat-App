882ed81cfff20ca810df30ac3c6589458ed1cd2a
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-07 04:32:21 +0530

    added the socket.io

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 var messages = [
@@ -18,9 +21,14 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body);
+    io.emit('message', req.body)
     res.sendStatus(200);
 });
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log("a user connected");
+});
+
+var server = http.listen(3000, () => {
     console.log("Server is listening on port : ", server.address().port)
 });

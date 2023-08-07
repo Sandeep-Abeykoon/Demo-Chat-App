e1d357bf5d97439b48c2f7a04354c2040c15ca62
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-07 10:07:36 +0530

    updated the code

--- server.js
+++ server.js
@@ -1,33 +1,71 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+var dbUrl = 'mongodb+srv://sandchanu:12345@cluster0.k0guq7z.mongodb.net/chatApp?retryWrites=true&w=majority'
 
-var messages = [
-    {name: "Tim", message: "Hi"},
-    {name: "Sandeep", message: "Happy birthday"}
-];
+
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
+
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}).exec()
+        .then(messages => {
+            res.send(messages);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
 });
 
+
+
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body)
-    res.sendStatus(200);
+    var message = new Message(req.body);
+    message.save()
+    .then(() => {
+        io.emit('message', req.body);
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        sendStatus(500);
+      });
 });
+
+
 
 io.on('connection', (socket) => {
     console.log("a user connected");
 });
+
+
+
+
+mongoose.connect(dbUrl)
+.then(() => {
+    console.log('Successfully connected');
+  })
+  .catch((err) => {
+    throw err;
+  });
+
+
+
 
 var server = http.listen(3000, () => {
     console.log("Server is listening on port : ", server.address().port)
